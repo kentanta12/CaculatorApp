a510c2d65175d1e4bdcc128ac4b81c10c535d4c6
kentanta12 <kentank12@gmail.com>
2020-06-27 16:04:32 +0900

    ラベル配置、ボタン配置、ラベルのOutlet接続、ボタンのAction接続

--- CaluclatorApp/ViewController.swift
+++ CaluclatorApp/ViewController.swift
@@ -9,7 +9,9 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var answerLabel: UILabel!
+    @IBOutlet weak var formulaLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +22,11 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func inputFormula(_ sender: UIButton) {
+    }
+    @IBAction func clearCalculation(_ sender: UIButton) {
+    }
+    @IBAction func calculateAnswer(_ sender: UIButton) {
+    }
 }
 
